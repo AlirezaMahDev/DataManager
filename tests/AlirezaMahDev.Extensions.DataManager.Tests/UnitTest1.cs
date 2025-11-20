73be8a0378089313e19192440ab6e684dfa676ae
alirezamahdev <alirezamahdev@outlook.com>
2025-11-20 15:09:18 +0300

    init base of project

---
+++ tests/AlirezaMahDev.Extensions.DataManager.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace AlirezaMahDev.Extensions.DataManager.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        using var access = new DataAccess("data.db");
+        var root = access.GetRoot();
+    }
+}
